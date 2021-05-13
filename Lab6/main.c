84a0515f7f826b00177710f0558456a64e947926
iolega <73232366+liolegal@users.noreply.github.com>
2021-05-13 11:40:41 +0300

    Add files via upload

---
+++ Lab6/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "TripleTree.h"
+
+
+int count_depth(struct Node * root);
+int Max(int a, int b, int c);
+
+
+int main() {
+
+    printf("\n");
+
+    struct Node * root = NULL;
+
+    // Inserting items to Tree
+    int i;
+    int x;
+    printf("Inserted items: ");
+    for(i = 0; i < 10; i++){
+        x = rand() % 20;
+        Insert(&root, x);
+        printf("%i ", x);
+    }
+    printf("\n");
+
+
+    // Finding number of levels
+    int Depth = count_depth(root);
+    printf("Numbers of tree's levels is - %i\n", Depth);
+
+    printf("\n");
+    system("pause");
+    return 0;
+}
+
+
+int Max(int a, int b, int c) {
+    if (a >= b && a >= c)
+        return a;
+    else if (b >= a && b >= c)
+        return b;
+    else
+        return c;
+}
+
+
+int count_depth(struct Node * root) {
+    if (!root)
+        return 0;
+
+    int left = count_depth(root->left);
+    int middle = count_depth(root->middle);
+    int right = count_depth(root->right);
+
+    return Max(left, middle, right) + 1;
+}
